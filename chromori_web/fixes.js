e99fa5424641544064bae91a6fd66bde87e027fb
cafeed28 <artlekseev@gmail.com>
2023-10-29 00:42:58 +0500

    web: move Scene_Boot patch

--- chromori_web/fixes.js
+++ chromori_web/fixes.js
@@ -7,16 +7,4 @@
   else if (document.body) document.body.appendChild(fpsMeterScript);
 }
 
-window.addEventListener("load", () => {
-  // FIXME: this doesn't work with OneLoader
-  // TODO: implement achievements (maybe)
-  // Even though this is a unlink from Steam, the key is still required to boot
-  Scene_Boot = class extends Scene_Boot {
-    hasSteamwork() {
-      return true;
-    }
-    getAchievementsData() {
-      return;
-    }
-  };
-});
+var global = globalThis;
