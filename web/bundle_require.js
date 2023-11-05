510efe3472a430770e0a5a5390c28ae4cbd2db73
cafeed28 <artlekseev@gmail.com>
2023-11-05 16:45:16 +0500

    update js-yaml

--- web/bundle_require.js
+++ web/bundle_require.js
@@ -1,3 +1,8 @@
+const jsYaml = require("js-yaml");
+jsYaml.safeDump = jsYaml.dump;
+jsYaml.safeLoad = jsYaml.load;
+jsYaml.safeLoadAll = jsYaml.loadAll;
+
 module.exports = {
   buffer: require("buffer"),
   crypto: require("crypto-browserify"),
@@ -8,5 +13,5 @@ module.exports = {
   fs: require("./require_fs"),
   path: require("./require_path"),
   "./js/libs/greenworks": require("./require_greenworks"),
-  "./js/libs/js-yaml-master": require("js-yaml"),
+  "./js/libs/js-yaml-master": jsYaml,
 };
