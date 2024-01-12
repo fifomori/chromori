4b3a9f61dae2e2c9560af47cc3be814ac0ad2bdf
cafeed28 <artlekseev@gmail.com>
2024-01-12 22:09:07 +0500

    i think i should atleast run my code before pushing it

--- backend/utils.js
+++ backend/utils.js
@@ -1,73 +1,79 @@
 const config = require("../config.json");
 
 const fs = require("fs");
+const fsp = require("fs/promises");
 const pp = require("path");
 const crypto = require("crypto");
 
-module.exports = {
-    encrypt(file) {
-        // copied from OneLoader
-        const iv = Buffer.from("EpicGamerMoment!");
-        const cipherStream = crypto.createCipheriv("aes-256-ctr", config.key, iv);
+function encrypt(file) {
+    // copied from OneLoader
+    const iv = Buffer.from("EpicGamerMoment!");
+    const cipherStream = crypto.createCipheriv("aes-256-ctr", config.key, iv);
 
-        return Buffer.concat([iv, cipherStream.update(Buffer.from(file, "utf8")), cipherStream.final()]);
+    return Buffer.concat([iv, cipherStream.update(Buffer.from(file, "utf8")), cipherStream.final()]);
+}
+
+function getDefaultPaths() {
+    switch (process.platform) {
+        case "win32":
+            return {
+                root: "C:\\Program Files (x86)\\Steam\\steamapps\\common\\OMORI",
+                config: pp.join(process.env.LOCALAPPDATA, "OMORI"),
+            };
+        default:
+            throw "Unsupported platform";
+    }
+}
+
+function validateConfig(config) {
+    const paths = getDefaultPaths();
+
+    return {
+        key: config.key,
+        gamePath: config.gamePath || paths.root,
+        gameDirectory: config.gameDirectory || "www",
+        argv: config.argv || [],
+        achievements: config.achievements || [],
+        noSteam: config.noSteam || false,
+    };
+}
+
+module.exports.encrypt = encrypt;
+module.exports.getDefaultPaths = getDefaultPaths;
+
+module.exports.config = {
+    saveSync(config) {
+        fs.writeFileSync(pp.join(process.cwd(), "config.json"), JSON.stringify(validateConfig(config), null, 4));
     },
-    getDefaultPaths() {
-        switch (process.platform) {
-            case "win32":
-                return {
-                    root: "C:\\Program Files (x86)\\Steam\\steamapps\\common\\OMORI",
-                    config: pp.join(process.env.LOCALAPPDATA, "OMORI"),
-                };
-            default:
-                throw "Unsupported platform";
+    async save(config) {
+        await fsp.writeFile(pp.join(process.cwd(), "config.json"), JSON.stringify(validateConfig(config), null, 4));
+    },
+};
+
+module.exports.fs = {
+    ...fsp,
+    createReadStream: fs.createReadStream,
+    async exists(path) {
+        try {
+            const file = await this.open(path, "r");
+            await file.close();
+            return true;
+        } catch (e) {
+            if (e.code !== "ENOENT") {
+                console.error(e);
+            }
+            return false;
         }
     },
-    config: {
-        _validate(config) {
-            const paths = this.getDefaultPaths();
-
-            return {
-                key: config.key,
-                gamePath: config.gamePath || paths.root,
-                gameDirectory: config.gameDirectory || "www",
-                argv: config.argv || [],
-                achievements: config.achievements || [],
-                noSteam: config.noSteam || false,
-            };
-        },
-        saveSync(config) {
-            fs.writeFileSync(pp.join(process.cwd(), "config.json"), JSON.stringify(_validate(config), null, 4));
-        },
-        async save(config) {
-            fs.writeFile(pp.join(process.cwd(), "config.json"), JSON.stringify(_validate(config), null, 4));
-        },
-    },
-    fs: {
-        ...require("fs/promises"),
-        createReadStream: fs.createReadStream,
-        async exists(path) {
-            try {
-                const file = await this.open(path, "r");
-                await file.close();
-                return true;
-            } catch (e) {
-                if (e.code !== "ENOENT") {
-                    console.error(e);
-                }
-                return false;
+    async isFile(path) {
+        try {
+            const stat = await this.stat(path);
+            return stat.isFile();
+        } catch (e) {
+            if (e.code !== "ENOENT") {
+                console.error(e);
             }
-        },
-        async isFile(path) {
-            try {
-                const stat = await this.stat(path);
-                return stat.isFile();
-            } catch (e) {
-                if (e.code !== "ENOENT") {
-                    console.error(e);
-                }
-                return false;
-            }
-        },
+            return false;
+        }
     },
 };
